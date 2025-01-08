fadfa631efd37e3c94b4282769646df8ea18b4cc
Laure <laure_ld@outlook.fr>
2025-01-08 17:23:38 +0100

    maj des chemin scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import React from 'react';
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import './styles/index.scss'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
