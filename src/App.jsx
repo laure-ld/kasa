9232fcde3bca325a7a267ad29806c1e4b1651d80
Laure <laure_ld@outlook.fr>
2025-01-11 17:33:57 +0100

    coorection des routes et du layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './styles/App.scss'
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
-import HomePage from './jsx/Accueil';
 import About from './jsx/apropos';
 import Header from './jsx/Header';
-import Main from './jsx/main';
+import Accueil from './jsx/Accueil';
 import PropTypes from 'prop-types';
 import Footer from './jsx/Footer';
 
@@ -12,25 +11,25 @@ import Footer from './jsx/Footer';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout><HomePage /></Layout>} />
-      <Route path="/about" element={<Layout><About /></Layout>} /> 
+      <Route path="/" element={<Layout><Accueil /></Layout>} />
+      <Route path="/accueil" element={<Layout><Accueil /></Layout>} />
+      <Route path="/apropos" element={<Layout><About /></Layout>} /> 
       <Route path="*" element={<h1>Erreur 404</h1>} />
     </ Routes>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired, // `node` signifie tout contenu JSX ou textuel
-};
-
-function Layout() {
+function Layout({children}) {
   return (
     <div>
       <header><Header /></header>
-      <main><Main /></main>
+      <main>{children}</main>
       <footer><Footer /></footer>
     </div>
   );
 }
+Layout.propTypes = {
+  children: PropTypes.node.isRequired, // `node` signifie tout contenu JSX ou textuel
+};
 
 export default App
