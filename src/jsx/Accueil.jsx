7346a9c145d9aaefa32c11103b13eb28ffe918fb
Laure <laure_ld@outlook.fr>
2025-01-11 17:33:28 +0100

    modification du nom main en accueil

--- src/jsx/Accueil.jsx
+++ src/jsx/Accueil.jsx
@@ -1,5 +1,22 @@
-function HomePage() {
+import React from "react";
+import Cards from "./Cards";
+import '../styles/index.scss';
+import data from '../appart.json';
+import Affiche from '../assets/Affiche.png';
 
+function Accueil() {
+    return (
+        <section>
+            <div className="Affiche">
+                <h1>Chez vous, partout et ailleurs</h1>
+                <img src={Affiche} alt="photo d'une falaise en bord de mer" />
+            </div>
+            <div className="gallery">
+                    {data.slice(0, 6).map((item) => (
+                        <Cards key={item.id} title={item.title} />
+                    ))}
+            </div>
+        </section>
+    );
 }
-
-export default HomePage
+export default Accueil;
