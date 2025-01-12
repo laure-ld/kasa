1e4243831aa8389602160ffb2886aaa1322b2e9b
Laure <laure_ld@outlook.fr>
2025-01-12 19:10:20 +0100

    maj de la pagee en utilisant la fct button

--- src/jsx/apropos.jsx
+++ src/jsx/apropos.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import paysage from '../assets/paysage.png';
 import '../styles/apropos.scss';
-import vite from '../assets/vite.png';
+import Button from './button';
+import React from "react";
 
 function About() {
+
     return (
         <section>
             <div className="Affiche">
@@ -13,19 +14,19 @@ function About() {
                 <nav>
                     <ul>
                         <li>
-                            <h3>Fiabilité<button ><img className='button' src={vite}></img></button></h3>
+                            <h3>Fiabilité<Button /></h3>
                             <p>Les annonces postées sur Kasa garatissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
                         </li>
                         <li>
-                            <h3>Respect<button ><img className='button' src={vite}></img></button></h3>
+                            <h3>Respect<Button /></h3>
                             <p>La bienveillance fait partie des valeurs fondtrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.</p>
                         </li>
                         <li>
-                            <h3>Service<button ><img className='button' src={vite}></img></button></h3>
+                            <h3>Service<Button /></h3>
                             <p>La qualité du service est au coeur  de notre engagement chez Kasa. Nous veillerons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance</p>
                         </li>
                         <li>
-                            <h3>Sécurité<button ><img className='button' src={vite}></img></button></h3>
+                            <h3>Sécurité<Button /></h3>
                             <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de vérifieer que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes</p>
                         </li>
                     </ul>
