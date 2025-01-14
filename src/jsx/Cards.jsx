1be016ebdb9a1103a54fca6c8f74c48708ef8ba7
Laure <laure_ld@outlook.fr>
2025-01-14 17:08:54 +0100

    Ajout recup id et <a> sur les cards

--- src/jsx/Cards.jsx
+++ src/jsx/Cards.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import PropTypes from 'prop-types';
 import '../styles/Main.scss';
 
-function Cards({title}) {
+
+function Cards({title, id}) {
+
+    const handleClick = () => {
+        window.localStorage.setItem("selectedId", id);
+        console.log(`ID stocké : ${id}`);
+    };
+
     return (
-        <div className="cards">
-            <h3 className="cards-title">{title}</h3>
-        </div>
+        <a href="/logement" onClick={handleClick}>
+            <div className="cards">
+                <h3 className="cards-title">{title}</h3>
+            </div>
+        </a>
     );
 }
-
 Cards.propTypes = {
     title: PropTypes.string.isRequired,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 export default Cards;
