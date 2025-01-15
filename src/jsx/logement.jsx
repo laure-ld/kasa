570a442da31be3ec6321978461db80c0389109a9
Laure <laure_ld@outlook.fr>
2025-01-15 16:50:30 +0100

    Ajout page infoLogement dans logement

--- src/jsx/logement.jsx
+++ src/jsx/logement.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ImgLogement from "./imgLogement";
+import InfoLogement from "./infoLogement";
 
 function Logement() {
     return (
         <div>
             <ImgLogement />
+            <InfoLogement />
         </div>
     )
 }
