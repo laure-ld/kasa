15e6b8b2986e925c769f6a3fcf70c43ed54de732
Laure <laure_ld@outlook.fr>
2025-01-16 17:15:07 +0100

    Arrangement du code et ajout id/className

--- src/jsx/infoLogement.jsx
+++ src/jsx/infoLogement.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import data from '../appart.json';
-import '../styles/logement.scss'
+import '../styles/logement.scss';
 
 function InfoLogement() {
     const id = window.localStorage.getItem("selectedId");
 
     return (
-        <section>
-            <div>
+        <section id="Container-info">
+            <div className="primary-info">
                 {data.map((item) => {
                     if (item.id == id) {
                         return <h1 key={item.id} id={item.id}>
@@ -23,14 +23,21 @@ function InfoLogement() {
                     }
                 })}
                 {data.map((item) => {
-                    if (item.id == id) {
-                        return <h3 key={item.id} id={item.id}>
-                                {item.tags}
-                                </h3>;   
+                    if (item.id === id) {
+                        return (
+                        <div key={item.id} id={item.id} className="tags">
+                            {item.tags.map((tag, index) => (
+                            <span key={index}>
+                                {tag}
+                            </span>
+                            ))}
+                        </div>
+                        );
                     }
-                })}
+                    return null;
+                    })}
             </div>
-            <div>
+            <div className="host">
                 {data.map((item) => {
                         if (item.id == id) {
                             return <h3 key={item.id} id={item.id}>
@@ -52,7 +59,7 @@ function InfoLogement() {
                         }
                 })}
             </div>
-            <div>
+            <div className="description">
             <h1>description</h1>
             {data.map((item) => {
                         if (item.id == id) {
@@ -62,15 +69,22 @@ function InfoLogement() {
                         }
                 })}
             </div>
-            <div>
+            <div className="équipement">
             <h1>Équipements</h1>
             {data.map((item) => {
-                        if (item.id == id) {
-                            return <h3 key={item.id} id={item.id}>
-                                    {item.equipments}
-                                    </h3>;   
-                        }
-                })}
+                    if (item.id === id) {
+                        return (
+                        <div key={item.id} id={item.id}>
+                            {item.equipments.map((equipments, index) => (
+                            <span key={index}>
+                                {equipments}
+                            </span>
+                            ))}
+                        </div>
+                        );
+                    }
+                    return null;
+                    })}
             </div>
         </section>
     )
