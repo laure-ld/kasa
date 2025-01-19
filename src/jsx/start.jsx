29a2d254e9eecd1628d05ae42a004ab4b5dd2659
Laure <laure_ld@outlook.fr>
2025-01-19 14:24:50 +0100

    Créaction fonctionnement des étoiles host

---
+++ src/jsx/start.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as starFilled } from "@fortawesome/free-solid-svg-icons";
+import { faStar as starEmpty } from "@fortawesome/free-regular-svg-icons";
+import PropTypes from  "prop-types";
+import '../styles/logement.scss';
+
+function RatingStars({ rating }) {
+    // Crée un tableau de 5 étoiles (pleines ou vides)
+    const stars = Array(5)
+        .fill()
+        .map((_, index) =>
+            index < rating ? (
+                <FontAwesomeIcon key={index} icon={starFilled} alt="Étoile pleine" />
+            ) : (
+                <FontAwesomeIcon key={index} icon={starEmpty} alt="Étoile vide" />
+            )
+        );
+
+    return <div className="rating-stars">{stars}</div>;
+}
+
+RatingStars.propTypes = {
+    rating: PropTypes.number.isRequired,
+};
+
+export default RatingStars;
