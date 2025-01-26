c152411868279762ac4c612e6de5f886ad03a892
Laure <laure_ld@outlook.fr>
2025-01-26 23:05:14 +0100

    Créaction du soulignement en fnt de la page

--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/header.scss';
 import Logo from '../assets/LOGO.png';
 
 function Header() {
+
     return (
         <header className="header">
             <img className="logo" src={Logo} alt="Logo Kasa" />
             <nav>
                 <ul>
-                    <li className="home" key="home">
-                        <a href="./accueil">Accueil</a>
+                    <li>
+                        <NavLink to="/accueil" className={({ isActive }) => (isActive ? "active" : "")}>
+                        Accueil
+                        </NavLink>
                     </li>
-                    <li className="about" key="about">
-                        <a href="./apropos">A propos</a>
+                    <li>
+                        <NavLink to="/apropos" className={({ isActive }) => (isActive ? "active" : "")}>
+                        A Propos
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
